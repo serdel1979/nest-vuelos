2ad85c404ff253bfda645961bc61aba3ada7a5b4
Sergio De Luca <sdlbsso@gmail.com>
2022-06-20 14:53:32 -0300

    Implementacion de interceptor y manejo de errores

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TomeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TomeOutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
