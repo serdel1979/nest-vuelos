8b8d8c9634ce7a5f95966001d5218c5425baa77b
Sergio De Luca <sdlbsso@gmail.com>
2022-06-20 15:24:41 -0300

    instalacion de validator y transformer, configuracion de interface y dto de usuario

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
@@ -5,9 +6,9 @@ import { TomeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TomeOutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
